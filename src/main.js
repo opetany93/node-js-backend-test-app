74af9342378d4830a1ffc5ab9c0662ebd078f3d8
opetany <bochynski.arkadiusz@gmail.com>
2019-08-13 15:43:26 +0200

    done, is ready for do tests
    - used express web framework
    - use of mariadb connector was changed from promise to callback
    - added get request handlers for imported sample database

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-const http = require('http');
-const mysql = require("mysql");
+// imports
+const mariadb = require('mariadb/callback');
 const fs = require('fs');
+const express = require('express');
 
+// constant variables
 const HOSTNAME = '127.0.0.1';
 const PORT = 8080;
 
@@ -14,26 +16,78 @@ try {
   console.error(err)
 }
 
-//Database connection
-let connection = mysql.createConnection({
-	host     : dbConfig.host,
-	user     : dbConfig.user,
-	password : dbConfig.password,
-	database : dbConfig.database
+const mariadbConn = mariadb.createConnection({
+	host : dbConfig.host,
+	user : dbConfig.user,
+  password : dbConfig.password,
+  database : dbConfig.database,
+  connectionLimit: 5
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
+const app = express();
+
+app.get('/customers', (request, respond) => {
+  console.log('Received a get customers request!');
+  mariadbConn.query("SELECT * FROM customers", (err, rows) => {
+    respond.send(rows);
+    console.log('Response on get customers request sent.');
+  });
 });
 
-module.exports = connection;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello dev.to!\n');
+app.get('/employees', (request, respond) => {
+  console.log('Received a get employees request!');
+  mariadbConn.query("SELECT * FROM employees", (err, rows) => {
+    respond.send(rows);
+    console.log('Response on get employees request sent.');
+  });
 });
 
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server running at ${HOSTNAME} on port ${PORT}.`);
+app.get('/offices', (request, respond) => {
+  console.log('Received a get offices request!');
+  mariadbConn.query("SELECT * FROM offices", (err, rows) => {
+    respond.send(rows);
+    console.log('Response on get offices request sent.');
+  });
 });
+
+app.get('/orderdetails', (request, respond) => {
+  console.log('Received a get orderdetails request!');
+  mariadbConn.query("SELECT * FROM orderdetails", (err, rows) => {
+    respond.send(rows);
+    console.log('Response on get orderdetails request sent.');
+  });
+});
+
+app.get('/orders', (request, respond) => {
+  console.log('Received a get orders request!');
+  mariadbConn.query("SELECT * FROM orders", (err, rows) => {
+    respond.send(rows);
+    console.log('Response on get orders request sent.');
+  });
+});
+
+app.get('/payments', (request, respond) => {
+  console.log('Received a get payments request!');
+  mariadbConn.query("SELECT * FROM payments", (err, rows) => {
+    respond.send(rows);
+    console.log('Response on get payments request sent.');
+  });
+});
+
+app.get('/productlines', (request, respond) => {
+  console.log('Received a get productlines request!');
+  mariadbConn.query("SELECT * FROM productlines", (err, rows) => {
+    respond.send(rows);
+    console.log('Response on get productlines request sent.');
+  });
+});
+
+app.get('/products', (request, respond) => {
+  console.log('Received a get products request!');
+  mariadbConn.query("SELECT * FROM products", (err, rows) => {
+    respond.send(rows);
+    console.log('Response on get products request sent.');
+  });
+});
+
+app.listen(PORT, () => console.log(`Node js server app listening on port ${PORT}.`));
